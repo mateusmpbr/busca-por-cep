a13d844bda55aeebf079cccb62f92e90fee2d787
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2020-04-09 19:59:11 -0300

    Extensão v1.0

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () { 
+    var $cep = $("#cep");
+    $cep.mask('00000-000');
+
+    $("#botao").bind("click", buscaCep)
+    $("#cep").bind("keypress", function(e){
+        if (e.which == 13) buscaCep();
+    })
+
+    function buscaCep(){
+        var valorCep = $("#cep").val();
+        var cep = valorCep.replace('-','');
+
+        $.getJSON (`https://viacep.com.br/ws/${cep}/json`, function(data){
+            var informacoes = "<tbody>"
+            $.each(data, function(chave, valor){
+                
+                if(!valor) valor = "N/A";
+                
+                informacoes = informacoes + `<tr><td><b>${chave}</b></td><td>${valor}</td></tr>`
+            });
+
+            var informacoes = informacoes + "</tbody>";
+
+            $("#minha-tabela").html(informacoes);
+        })
+    }
+
+});
